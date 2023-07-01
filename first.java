4c4f2a6beacb9924f481dff7204d6f446ccb5120
EvilHydra09 <abhidnya09@gmail.com>
2023-07-01 18:58:26 +0530

    hello

---
+++ first.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class first{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int n = 0;
+        Scanner sc = new Scanner(System.in);
+        int j = sc.nextInt();
+        System.out.println("Enter no is "+j);
+    }
+}
